5b154822dfe7f9beb491afcebe0ad529e519a868
David Golden <xdg@xdg.me>
2017-11-11 23:20:37 -0500

    Add tests for Nil checks on interface values

--- testy_test.go
+++ testy_test.go
@@ -7,9 +7,11 @@
 package testy_test
 
 import (
-	"github.com/xdg/testy"
+	"errors"
 	"regexp"
 	"testing"
+
+	"github.com/xdg/testy"
 )
 
 func TestHelpers(t *testing.T) {
@@ -24,6 +26,8 @@ func TestHelpers(t *testing.T) {
 	test.NotNil(test)
 	test.Nil(aNil)
 	test.Nil(nilSlice)
+	test.Nil(error(nil))
+	test.NotNil(errors.New("an error"))
 
 	// failures on lines 29+; check later
 	test.True(false)
@@ -37,6 +41,8 @@ func TestHelpers(t *testing.T) {
 	test.Equal(nil, test)
 	test.Unequal(nilSlice, nil)
 	test.Unequal(nil, nilSlice)
+	test.Nil(errors.New("an error"))
+	test.NotNil(error(nil))
 
 	// Uncomment for diagnostic output
 	// 	for i, s := range test.Output() {
@@ -44,19 +50,19 @@ func TestHelpers(t *testing.T) {
 	// 	}
 
 	fc := test.FailCount()
-	expect := 11
+	expect := 13
 	if fc != expect {
 		t.Errorf("Incorrect FailCount. Got %d, but expected %d", fc, expect)
 	}
 
 	output := test.Output()
-	if ok, _ := regexp.MatchString("testy_test.go:29: Expression was not true", output[0]); !ok {
+	if ok, _ := regexp.MatchString("testy_test.go:33: Expression was not true", output[0]); !ok {
 		t.Errorf("True() had wrong error message: '%s'", output[0])
 	}
-	if ok, _ := regexp.MatchString("testy_test.go:30: Expression was not false", output[1]); !ok {
+	if ok, _ := regexp.MatchString("testy_test.go:34: Expression was not false", output[1]); !ok {
 		t.Errorf("False() had wrong error message: '%s'", output[1])
 	}
-	if ok, _ := regexp.MatchString("testy_test.go:31: Values were not equal", output[2]); !ok {
+	if ok, _ := regexp.MatchString("testy_test.go:35: Values were not equal", output[2]); !ok {
 		t.Errorf("Equal() had wrong error message: '%s'", output[2])
 	}
 	if ok, _ := regexp.MatchString(`(?m)^\s+Got: 1 \(int\)`, output[2]); !ok {
@@ -65,35 +71,41 @@ func TestHelpers(t *testing.T) {
 	if ok, _ := regexp.MatchString(`(?m)^\s+Wanted: 2 \(int\)`, output[2]); !ok {
 		t.Errorf("Equal() had wrong 'got' message: '%s'", output[2])
 	}
-	if ok, _ := regexp.MatchString("testy_test.go:32: Values were not unequal", output[3]); !ok {
+	if ok, _ := regexp.MatchString("testy_test.go:36: Values were not unequal", output[3]); !ok {
 		t.Errorf("Unequal() had wrong error message: '%s'", output[3])
 	}
 	if ok, _ := regexp.MatchString(`(?m)^\s+Both: "foo"`, output[3]); !ok {
 		t.Errorf("Unequal() had wrong 'got' message: '%s'", output[3])
 	}
-	if ok, _ := regexp.MatchString("testy_test.go:33: Values were not unequal", output[4]); !ok {
+	if ok, _ := regexp.MatchString("testy_test.go:37: Values were not unequal", output[4]); !ok {
 		t.Errorf("Unequal() had wrong error message: '%s'", output[4])
 	}
 	if ok, _ := regexp.MatchString(`(?m)^\s+Both: true`, output[4]); !ok {
 		t.Errorf("Unequal() had wrong 'got' message: '%s'", output[4])
 	}
-	if ok, _ := regexp.MatchString("testy_test.go:34: Expression was not nil", output[5]); !ok {
+	if ok, _ := regexp.MatchString("testy_test.go:38: Expression was not nil", output[5]); !ok {
 		t.Errorf("Nil() had wrong error message: '%s'", output[5])
 	}
-	if ok, _ := regexp.MatchString("testy_test.go:35: Expression was nil", output[6]); !ok {
+	if ok, _ := regexp.MatchString("testy_test.go:39: Expression was nil", output[6]); !ok {
 		t.Errorf("NotNil() had wrong error message: '%s'", output[6])
 	}
-	if ok, _ := regexp.MatchString(`testy_test.go:36: Can't safely compare nil values for equality`, output[7]); !ok {
+	if ok, _ := regexp.MatchString(`testy_test.go:40: Can't safely compare nil values for equality`, output[7]); !ok {
 		t.Errorf("Equal() had wrong error message: '%s'", output[7])
 	}
-	if ok, _ := regexp.MatchString(`testy_test.go:37: Can't safely compare nil values for equality`, output[8]); !ok {
+	if ok, _ := regexp.MatchString(`testy_test.go:41: Can't safely compare nil values for equality`, output[8]); !ok {
 		t.Errorf("Equal() had wrong error message: '%s'", output[8])
 	}
-	if ok, _ := regexp.MatchString(`testy_test.go:38: Can't safely compare nil values for equality`, output[9]); !ok {
+	if ok, _ := regexp.MatchString(`testy_test.go:42: Can't safely compare nil values for equality`, output[9]); !ok {
 		t.Errorf("Unequal() had wrong error message: '%s'", output[9])
 	}
-	if ok, _ := regexp.MatchString(`testy_test.go:39: Can't safely compare nil values for equality`, output[10]); !ok {
+	if ok, _ := regexp.MatchString(`testy_test.go:43: Can't safely compare nil values for equality`, output[10]); !ok {
 		t.Errorf("Unequal() had wrong error message: '%s'", output[10])
+	}
+	if ok, _ := regexp.MatchString("testy_test.go:44: Expression was not nil", output[11]); !ok {
+		t.Errorf("Nil() had wrong error message: '%s'", output[11])
+	}
+	if ok, _ := regexp.MatchString("testy_test.go:45: Expression was nil", output[12]); !ok {
+		t.Errorf("NotNil() had wrong error message: '%s'", output[12])
 	}
 }
 
@@ -104,7 +116,7 @@ func TestLabelUplevel(t *testing.T) {
 	checkEven(test, 3) // Line 104; set below
 
 	output := test.Output()
-	if ok, _ := regexp.MatchString(`testy_test.go:104: Testing 3: Value is not even`, output[0]); !ok {
+	if ok, _ := regexp.MatchString(`testy_test.go:116: Testing 3: Value is not even`, output[0]); !ok {
 		t.Errorf("checkEven() had wrong error message: '%s'", output[0])
 	}
 
